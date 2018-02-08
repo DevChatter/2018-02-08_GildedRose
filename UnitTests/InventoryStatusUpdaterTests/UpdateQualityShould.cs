b32ab30bd71c08b00cb7a5eb8da509825b5fdad3
Brendan Enrick <brendan@enrick.com>
2018-02-08 13:14:29 -0500

    adding normal item tests

--- UnitTests/InventoryStatusUpdaterTests/UpdateQualityShould.cs
+++ UnitTests/InventoryStatusUpdaterTests/UpdateQualityShould.cs
@@ -4,10 +4,10 @@ using Xunit;
 
 namespace UnitTests.InventoryStatusUpdaterTests
 {
-    public class UpdateQualityShould
+    public class UpdateQualityShould_GivenNormalItem
     {
         [Fact]
-        public void RunWithoutError_GivenEmptyCollection()
+        public void RunWithoutError()
         {
             var statusUpdater = new InventoryStatusUpdater();
 
@@ -15,15 +15,67 @@ namespace UnitTests.InventoryStatusUpdaterTests
         }
 
         [Fact]
-        public void LowerQuality_GivenNormalItem()
+        public void LowerQuality()
         {
             var statusUpdater = new InventoryStatusUpdater();
-
             var normalItem = new Item { Name = "Normal Normal", Quality = 20, SellIn = 10 };
             var items = new List<Item> {normalItem};
+
             statusUpdater.UpdateQuality(items);
 
             Assert.Equal(19, normalItem.Quality);
+        }
+
+        [Fact]
+        public void LowerSellIn()
+        {
+            var statusUpdater = new InventoryStatusUpdater();
+            var normalItem = new Item { Name = "Normal Normal", Quality = 20, SellIn = 10 };
+            var items = new List<Item> {normalItem};
+
+            statusUpdater.UpdateQuality(items);
+
+            Assert.Equal(9, normalItem.SellIn);
+        }
+
+        [Fact]
+        public void FloorQualityAtZero()
+        {
+            var statusUpdater = new InventoryStatusUpdater();
+            var normalItem = new Item { Name = "Normal Normal", Quality = 1, SellIn = 10 };
+            var items = new List<Item> {normalItem};
+
+            statusUpdater.UpdateQuality(items);
+            statusUpdater.UpdateQuality(items);
+
+            Assert.Equal(0, normalItem.Quality);
+        }
+
+        [Fact]
+        public void AllowNegativeSellIn()
+        {
+            var statusUpdater = new InventoryStatusUpdater();
+            var normalItem = new Item { Name = "Normal Normal", Quality = 50, SellIn = 1 };
+            var items = new List<Item> {normalItem};
+
+            statusUpdater.UpdateQuality(items);
+            statusUpdater.UpdateQuality(items);
+
+            Assert.Equal(-1, normalItem.SellIn);
+        }
+
+        [Fact]
+        public void DecreaseQualityTwiceAsFastAfterSellInReachesZero()
+        {
+            var statusUpdater = new InventoryStatusUpdater();
+            var normalItem = new Item { Name = "Normal Normal", Quality = 50, SellIn = 1 };
+            var items = new List<Item> {normalItem};
+
+            statusUpdater.UpdateQuality(items); // -1
+            statusUpdater.UpdateQuality(items); // -2
+            statusUpdater.UpdateQuality(items); // -2
+
+            Assert.Equal(45, normalItem.Quality);
         }
     }
 }
