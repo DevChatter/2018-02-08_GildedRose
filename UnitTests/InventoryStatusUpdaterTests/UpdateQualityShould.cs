60b515612a28a6eac0057a955a3b0621e12fb2f1
Brendan Enrick <brendan@enrick.com>
2018-02-08 12:59:18 -0500

    adding initial assumption tests

--- UnitTests/InventoryStatusUpdaterTests/UpdateQualityShould.cs
+++ UnitTests/InventoryStatusUpdaterTests/UpdateQualityShould.cs
@@ -13,5 +13,17 @@ namespace UnitTests.InventoryStatusUpdaterTests
 
             statusUpdater.UpdateQuality(new List<Item>());
         }
+
+        [Fact]
+        public void LowerQuality_GivenNormalItem()
+        {
+            var statusUpdater = new InventoryStatusUpdater();
+
+            var normalItem = new Item { Name = "Normal Normal", Quality = 20, SellIn = 10 };
+            var items = new List<Item> {normalItem};
+            statusUpdater.UpdateQuality(items);
+
+            Assert.Equal(normalItem.Quality, normalItem.Quality - 1);
+        }
     }
 }
